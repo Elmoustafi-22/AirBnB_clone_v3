c10a2ff0699c5e9c06d67a6a35533cda34c2286e
root <mustophaqadir@gmail.com>
2024-02-27 19:18:56 -0800

    fixed pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
+"""Route to return the status of your API"""
 import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
 
+
+# creating a Flask app
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-app.register_blueprint(app_views, url_prefix="/api/v1")
 
-@app.teardown_appcontext
-def close(ctx):
-    storage.close()
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.errorhandler(404)
@@ -24,7 +23,12 @@ def page_not_found(error):
 @app.errorhandler(400)
 def page_not_found(error):
     message = error.description
-    return mesage, 400
+    return message, 400
+
+
+@app.teardown_appcontext
+def close(ctx):
+    storage.close()
 
 
 if os.getenv("HBNB_API_HOST"):
@@ -33,8 +37,7 @@ else:
     host = "0.0.0.0"
 
 if os.getenv("HBNB_API_PORT"):
-    port = os.getenv("HBNB_API_PORT")
-     
+    port = int(os.getenv("HBNB_API_PORT"))
 else:
     port = 5000
 
